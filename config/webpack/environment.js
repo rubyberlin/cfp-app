674ba3619d766c828c74bc6748d439ef6e08e399
Alex Coles <alex@alexbcoles.com>
2018-05-23 18:39:17 +0100

    Install rails-erb-loader with generated config
        ./bin/rails webpacker:install:erb

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,5 @@
 const { environment } = require('@rails/webpacker')
+const erb = require('./loaders/erb')
 const webpack = require('webpack')
 
 environment.loaders.append('jqueryLibs', {
@@ -54,4 +55,5 @@ environment.plugins.append(
   })
 )
 
+environment.loaders.append('erb', erb)
 module.exports = environment
