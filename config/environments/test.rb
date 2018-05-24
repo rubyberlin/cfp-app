4188e21356b1a23f4ad2ad667932ee72ed9aeab4
Alex Coles <alex@alexbcoles.com>
2018-05-24 22:29:39 +0100

    Update Rails config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => 'public, max-age=3600'
+    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
 
   # Show full error reports and disable caching.
@@ -29,17 +29,16 @@ Rails.application.configure do
   config.action_controller.allow_forgery_protection = false
 
   config.action_mailer.default_url_options = { host: 'http://test.host' }
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.default_options = { from: 'cfp@example.org' }
+  # Store uploaded files on the local file system in a temporary directory
+  config.active_storage.service = :test
+
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-
-  # Randomize the order test cases are executed.
-  config.active_support.test_order = :random
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
